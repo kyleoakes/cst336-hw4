2be417157c341407aa7e9b6223edbfe953509225
KyleOakes <koakes@csumb.edu>
2020-07-22 05:57:19 +0000

    added a randomly generated company name and slogan to the footer

--- app.js
+++ app.js
@@ -3,17 +3,34 @@ const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
+// var jQuery = require('jquery');
+// var $ = jQuery;
+
+var faker = require('faker');
+var companyName, companyText;
+
+function updateCompany() {
+   companyName = faker.company.companyName();
+   companyText = faker.company.bsBuzz() + " " + faker.company.bsAdjective() + " " + faker.company.bsNoun() + "!";
+   app.locals.companyName = companyName;
+   app.locals.companyText = companyText;
+}
+
 // routes
 app.get("/", function(req, res){
-   res.render("index.ejs"); 
+   updateCompany();
+   res.render("index.ejs");
 });
 app.get("/gates", function(req, res){
+   updateCompany();
    res.render("gates.ejs"); 
 });
 app.get("/binary_adder", function(req, res){
+   updateCompany();
    res.render("binary_adder.ejs"); 
 });
 app.get("/references", function(req, res){
+   updateCompany();
    res.render("references.ejs"); 
 });
 
